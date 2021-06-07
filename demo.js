a1399920e6aba85ec0652cf58efd02885a46ec0b
rbunge-nsc <52756750+rbunge-nsc@users.noreply.github.com>
2021-06-07 15:42:29 -0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,6 @@
+function changeMsg() {
+    var input = document.theForm.elements[0].value;
+    document.getElementById("msg").innerHTML = input;
+    //document.getElementById("msg").innerHTML = "test";
+    //return input;
+}
